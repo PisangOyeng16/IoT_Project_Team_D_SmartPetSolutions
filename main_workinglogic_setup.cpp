9cc412007ccdf6c28f467edf4327f72f3969b517
PisangOyeng16 <hafiz.indriawan@gmail.com>
2025-12-13 16:40:27 +0900

    initialize new files in new branch

---
+++ main_workinglogic_setup.cpp
@@ -0,0 +1,7 @@
+// === Pet Feeder (ESP32) ===
+// Pins: HX711 DT=4, SCK=5 | Servo=18 | I2C SDA=21, SCL=22 | Buttons: 12/13/14/15 (to GND)
+// Power: ESP32+HX711 @3.3V; RTC+LCD+Servo @5V (common GND)
+
+// This is the program code for the Pet Feeder device based on ESP32 microcontroller working logic.
+
+// This script is a part of a larger project and is responsible for setting up the working logic of the machine.
